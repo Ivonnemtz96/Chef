48c9e6e963833ada3f51d0b48d70b4eddfb073c8
Ivonne Mtz <54880593+Ivonnemtz96@users.noreply.github.com>
2023-10-16 13:23:48 -0700

    cambios en admin

--- admin/backend/cerrar_sesion.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-session_destroy(); // Destruir todas las variables de sesión
-
-header("Location: /admin/login/"); // Redirigir a la página de inicio de sesión
-exit();
-?>
--- admin/modules/entradas.php
+++
@@ -1,7 +0,0 @@
-<div class="page" style="margin-top: 10rem;">
-    <div class="row clearfix">
-        <?
-            require_once($_SERVER["DOCUMENT_ROOT"]."/admin/backend/ver_entradas.php");
-        ?>
-    </div>
-</div>
