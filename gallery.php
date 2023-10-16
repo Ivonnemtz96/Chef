0e04155319cc0c746f19495e80f9c590133d2dbd
Ivonne Mtz <54880593+Ivonnemtz96@users.noreply.github.com>
2023-10-16 13:24:06 -0700

    admin se sube

---
+++ gallery.php
@@ -0,0 +1,35 @@
+<?php 
+  include('admin/backend/indexcontroller.php');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<?php
+    include('includes/head.php');
+    include('includes/metricas.php');
+?>
+<?
+if (isset($_GET)) {
+  include_once("php/msg.php");
+}
+?>
+<body class="hidden-bar-wrapper">
+    <?
+    include('includes/metricasBody.php');
+    ?>
+    <div class="page-wrapper">
+        <?php
+            include('includes/preloader.php');
+            include('modules/'.$lenguaje.'/header.php');
+            include('modules/'.$lenguaje.'/gallery.php');
+            include('modules/'.$lenguaje.'/footer.php');
+            
+        ?>
+    </div>
+    <?
+        include('includes/scripts.php');
+        include('includes/redes.php');   
+    ?>
+
+</body>
+
+</html>
---
+++ modules/en/gallery.php
@@ -0,0 +1,114 @@
+<!-- Page Title -->
+<section class="page-title" style="background-image: url(/images/background/7.jpg)">
+    <div class="auto-container">
+        <h1>Gallery</h1>
+        <div class="text">Dare to discover the best of Los Cabos right at your home</div>
+    </div>
+</section>
+<!--End Page Title-->
+
+<!-- Gallery Section Three -->
+<section class="gallery-section-three">
+    <div class="auto-container">
+        <div class="masonry-items-container row clearfix">
+
+            <!-- Gallery Block Five -->
+            <div class="gallery-block-five masonry-item col-lg-4 col-md-6 col-sm-12">
+                <div class="inner-box">
+                    <div class="image">
+                        <img src="/images/gallery/18.jpg?=1" alt="" />
+                        <div class="overlay-box">
+                            <a href="/images/gallery/18.jpg?=1" class="lightbox-image plus flaticon-eye-1"></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Gallery Block Five -->
+            <div class="gallery-block-five masonry-item col-lg-4 col-md-6 col-sm-12">
+                <div class="inner-box">
+                    <div class="image">
+                        <img src="/images/gallery/19.jpg?=1" alt="" />
+                        <div class="overlay-box">
+                            <a href="/images/gallery/19.jpg?=1" class="lightbox-image plus flaticon-eye-1"></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Gallery Block Five -->
+            <div class="gallery-block-five masonry-item col-lg-4 col-md-6 col-sm-12">
+                <div class="inner-box">
+                    <div class="image">
+                        <img src="/images/gallery/21.jpg?=1" alt="" />
+                        <div class="overlay-box">
+                            <a href="/images/gallery/21.jpg?=1" class="lightbox-image plus flaticon-eye-1"></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Gallery Block Five -->
+            <div class="gallery-block-five masonry-item col-lg-4 col-md-6 col-sm-12">
+                <div class="inner-box">
+                    <div class="image">
+                        <img src="/images/gallery/22.jpg?=1" alt="" />
+                        <div class="overlay-box">
+                            <a href="/images/gallery/22.jpg?=1" class="lightbox-image plus flaticon-eye-1"></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Gallery Block Five -->
+            <div class="gallery-block-five masonry-item col-lg-8 col-md-12 col-sm-12">
+                <div class="inner-box">
+                    <div class="image">
+                        <img src="/images/gallery/23.jpg?=1" alt="" />
+                        <div class="overlay-box">
+                            <a href="/images/gallery/23.jpg?=1" class="lightbox-image plus flaticon-eye-1"></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Gallery Block Five -->
+            <div class="gallery-block-five masonry-item col-lg-4 col-md-6 col-sm-12">
+                <div class="inner-box">
+                    <div class="image">
+                        <img src="/images/gallery/24.jpg?=1" alt="" />
+                        <div class="overlay-box">
+                            <a href="/images/gallery/24.jpg?=1" class="lightbox-image plus flaticon-eye-1"></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Gallery Block Five -->
+            <div class="gallery-block-five masonry-item col-lg-4 col-md-6 col-sm-12">
+                <div class="inner-box">
+                    <div class="image">
+                        <img src="/images/gallery/25.jpg?=1" alt="" />
+                        <div class="overlay-box">
+                            <a href="/images/gallery/25.jpg?=1" class="lightbox-image plus flaticon-eye-1"></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Gallery Block Five -->
+            <div class="gallery-block-five masonry-item col-lg-4 col-md-6 col-sm-12">
+                <div class="inner-box">
+                    <div class="image">
+                        <img src="/images/gallery/26.jpg?=1" alt="" />
+                        <div class="overlay-box">
+                            <a href="/images/gallery/26.jpg?=1" class="lightbox-image plus flaticon-eye-1"></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</section>
+<!-- End Gallery Section Three -->
